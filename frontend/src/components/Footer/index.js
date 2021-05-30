120ed53b37d045f4f55bd3b576b4f4bf739dc3e5
Ryan <DaltonR121@gmail.com>
2021-05-29 22:02:36 -0400

    Added favicon and linked github

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 function Footer() {
   return(
-    <div className="footer__container">
-      <h1>Made By: Ryan Dalton</h1>
-    </div>
+    <a href="https://github.com/DaltonR121">
+      <div className="footer__container">
+        <img src={require('../../images/github.png').default} alt="Github Logo" />
+        <h1>Made By: Ryan Dalton</h1>
+      </div>
+    </a>
   );
 }
 
